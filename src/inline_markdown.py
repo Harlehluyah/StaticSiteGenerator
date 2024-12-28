c1e64ff56cd5390769448a4930da1bef15843a66
Harlehluyah <marrcuss00@gmail.com>
2024-12-29 02:10:52 +0800

    7. Added extract_markdown_images & extract_markdown_links, test cases made for them, Regex used

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -20,3 +21,14 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
     return new_nodes
+
+def extract_markdown_images(text):
+    description_images_tuple_list = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)    
+
+    return description_images_tuple_list
+
+def extract_markdown_links(text):
+    description_link_tuple_list = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    
+    return description_link_tuple_list
+    
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,10 +1,13 @@
 import unittest
-from inline_markdown import (split_nodes_delimiter,)
+from inline_markdown import (split_nodes_delimiter, extract_markdown_images, extract_markdown_links)
 
 from textnode import TextNode, TextType
 
 
 class TestInlineMarkdown(unittest.TestCase):
+    
+#   ----- Split_node_delimiter Tests -------------------------------------------------------
+    
     def test_delim_bold(self):
         node = TextNode("This is text with a **bolded** word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
@@ -80,6 +83,58 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes,
         )
 
+#   ----- extract_markdown_images(text) Tests ----------------------------------------------------
+
+    def test_single_image(self):
+        text = "![alt text](https://example.com/image.jpg)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [("alt text", "https://example.com/image.jpg")])
+    
+    def test_no_image(self):
+        text = "Plain text with no images"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [])
+    
+    def test_multiple_images(self):
+        text = "![cat](cat.jpg) ![dog](dog.jpg)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [("cat", "cat.jpg"), ("dog", "dog.jpg")])
+        
+    def test_image_with_spaces(self):
+        text = "![my cool cat](https://example.com/cool%20cat.jpg)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [("my cool cat", "https://example.com/cool%20cat.jpg")])
+    
+    def test_mixed_content(self):
+        text = "Some text ![image](img.jpg) more text ![another](pic.png)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [("image", "img.jpg"), ("another", "pic.png")])
+        
+
+#   ----- extract_markdown_links(text) Tests ------------------------------------------------
+
+    def test_single_link(self):
+        text = "[Boot.dev](https://boot.dev)"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [("Boot.dev", "https://boot.dev")])
+    
+    def test_no_links(self):
+        text = "Plain text with no links"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [])
+
+    def test_multiple_links(self):
+        text = "Check out [Boot.dev](https://boot.dev) and [Python](https://python.org)"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [("Boot.dev", "https://boot.dev"), ("Python", "https://python.org")])
+        
+    def test_ignore_images(self):
+        text = "[link](https://boot.dev) ![image](pic.jpg)"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [("link", "https://boot.dev")])
+        
+        
+        
 
 if __name__ == "__main__":
     unittest.main()
